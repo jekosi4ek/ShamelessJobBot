1d8f0c9c2f4314a6b404f897f8ec9207c1d9d075
Ievgen Koleinyk <ievgen.koleinyk@namecheap.com>
2025-11-26 12:42:38 +0200

    Update scrapper.py

--- scrapper.py
+++ scrapper.py
@@ -217,7 +217,7 @@ if __name__ == "__main__":
     scrape()
 
     # далі кожну хвилину
-    schedule.every(3).minutes.do(scrape)
+    schedule.every(5).minutes.do(scrape)
 
     while True:
         schedule.run_pending()
