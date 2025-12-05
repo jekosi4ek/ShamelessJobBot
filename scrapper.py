151585994e785846ea559232bbb9d4cec0b389e3
Ievgen Koleinyk <ievgen.koleinyk@namecheap.com>
2025-12-05 18:40:50 +0200

    Update scrapper.py

--- scrapper.py
+++ scrapper.py
@@ -267,7 +267,22 @@ def scrape():
                 if str(pretty["ID Компанії"]) == "555":
                     send_to_telegram(message, CHAT_ID_PARTY)
 
-            updates.append(pretty)
+            updates.append({
+                "date": pretty.get("Дата") or None,
+                "position_id": pretty["ID Позиції"],
+                "name": pretty["Назва"],
+                "company": pretty["Компанія"],
+                "company_id": pretty["ID Компанії"],
+                "working_hours": pretty["Час"],
+                "location": pretty["Локація"],
+                "role_id": pretty["ID Role"],
+                "profession": pretty["Професія"],
+                "free_capacity": pretty["freeCapacity"],
+                "total_capacity": pretty["totalCapacity"],
+                "wage_hour": pretty["Оплата (година)"],
+                "wage_fix": pretty["Оплата (фікс)"],
+                "scrapped_at": pretty["scrapped_at"],
+            })
 
         # save to DB
         if updates:
@@ -318,22 +333,7 @@ def scrape():
                     wage_hour = EXCLUDED.wage_hour,
                     wage_fix = EXCLUDED.wage_fix,
                     scrapped_at = EXCLUDED.scrapped_at;
-            """), {
-                "date": pretty.get("Дата") or None,
-                "position_id": pretty["ID Позиції"],
-                "name": pretty["Назва"],
-                "company": pretty["Компанія"],
-                "company_id": pretty["ID Компанії"],
-                "working_hours": pretty["Час"],
-                "location": pretty["Локація"],
-                "role_id": pretty["ID Role"],
-                "profession": pretty["Професія"],
-                "free_capacity": pretty["freeCapacity"],
-                "total_capacity": pretty["totalCapacity"],
-                "wage_hour": pretty["Оплата (година)"],
-                "wage_fix": pretty["Оплата (фікс)"],
-                "scrapped_at": datetime.utcnow()
-            })
+            """),updates)
             db.commit()
 
             # print(json.dumps(pretty, ensure_ascii=False, indent=4))
